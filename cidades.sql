94335e6dc760599320164919bde7029ddb0c4112
vitoriavivian227@gmail.com <maciel.vivian@alunoescolassociais.org.br>
2021-09-01 17:29:10 -0300

    vaiGO

---
+++ cidades.sql
@@ -0,0 +1,13 @@
+INSERT INTO cidades 
+    (nome, estado, area)
+VALUES
+    ('Manaus', 'Amazonas', 11.401),
+    ('Fortaleza', 'Ceará', 313.8),
+    ('Rio de Janeio', 'Rio de Janeiro', 1.255),
+    ('Presidente Prudente', 'São Paulo', 562.794),
+    ('Colatina', 'Espírito Santo', 1.417),
+    ('Londrina', 'Paraná', 1.651),
+    ('Ibiporã', 'Paraná', 300.187),
+    ('Juazeiro', 'Bahia', 6.501),
+    ('Cambé', 'Paraná', 495.4),
+    ('São Paulo', 'São Paulo', 1.521);
---
+++ insertDadosEstado.sql
@@ -0,0 +1,7 @@
+INSERT INTO estados
+    (nome, sigla, regiao, populacao)
+VALUES
+    ('Acre','AC','norte',0.83);
+
+
+    SELECT * FROM `estados` 
